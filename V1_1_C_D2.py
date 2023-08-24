2503265de667476500e503b0d391abea0a5efaea
Milos Petrovic <milos@sidekickhealth.com>
2023-08-24 22:30:30 +0000

    convert metric to imperial speed

---
+++ V1_1_C_D2.py
@@ -0,0 +1,10 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 24.08.2023
+# Verkefni: Forritun 1A - V1_1_A_D2
+# Athugasemdir: Ekki athugasemdir.
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+metricSpeed = float(input("Hraði í metrar/sek: "))
+imperialSpeed = metricSpeed * 3600 / 1609
+print("Það eru", round(imperialSpeed, 2), "mílur/klst")
