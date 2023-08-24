894ae2feb7aa72b8754e0f1cc6e8cd78270c2abb
Milos Petrovic <milos@sidekickhealth.com>
2023-08-24 21:04:14 +0000

    Calculating the area of a rectangle

---
+++ V1_1_C_D1.py
@@ -0,0 +1,10 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 24.08.2023
+# Verkefni: Forritun 1A - V1_1_C_D1
+# Athugasemdir: Ekki athugasemdir.
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+first_number = float(input("Lengd ferhyrnings: "))
+second_number = float(input("Breidd ferhyrnings: "))
+print("Flatarmál ferhyrnings er: ", first_number * second_number)
