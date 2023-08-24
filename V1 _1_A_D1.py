29c4c5dcc55082b9454ddf90242a636cb9d0ec53
Milos Petrovic <milos@sidekickhealth.com>
2023-08-24 20:45:36 +0000

    Submiting V1-1-A-D1 assignment

---
+++ V1 _1_A_D1.py
@@ -0,0 +1,10 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 24.08.2023
+# Verkefni: Forritun 1A - V1_1_A_D1
+# Athugasemdir: Ekki athugasemdir
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+first_integer = int(input("Fyrri heiltalan: "))
+second_integer = int(input("Seinni heiltalan: "))
+print("Fyrri talan er", first_integer, "Seinni talan er", second_integer)
