4fd2ee570100bdcb0b94ac89686c4dbf08a28143
Milos Petrovic <milos@sidekickhealth.com>
2023-08-24 21:21:49 +0000

    calculating bmi

---
+++ V1_1_D_D1.py
@@ -0,0 +1,10 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 24.08.2023
+# Verkefni: Forritun 1A - V1_1_C_D1
+# Athugasemdir: Ekki athugasemdir.
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+height = float(input("Hæð í metrum: "))
+weight = float(input("Þyngd í kg: "))
+print("BMI: ", round(weight / (height * height), 2))
