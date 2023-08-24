4ef7114fbdb4ca4a9458157d1266731e51f571f5
Milos Petrovic <milos@sidekickhealth.com>
2023-08-24 20:57:39 +0000

    sum of two float numbers

---
+++ V1_1_B_D1.py
@@ -0,0 +1,10 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 24.08.2023
+# Verkefni: Forritun 1A - V1_1_B_D1
+# Athugasemdir: Ekki athugasemdir.
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+first_number = float(input("Fyrri tala: "))
+second_number = float(input("Seinni tala: "))
+print("Summa talnanna er:", first_number + second_number)
