096562440fbe9275f640c9be6c440d5be5e99967
Milos Petrovic <milos@sidekickhealth.com>
2023-08-26 14:42:46 +0000

    day in week

---
+++ V2_1_A_D1.py
@@ -0,0 +1,25 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 26.08.2023
+# Verkefni: Forritun 1A - V2_1_A_D1
+# Athugasemdir: Ekki athugasemdir
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+dayInWeek = int(input("Númer dags: "))
+
+if dayInWeek == 1:
+  print("Sunnudagur")
+elif dayInWeek == 2:
+  print("Mánudagur")
+elif dayInWeek == 3:
+  print("Þriðjudagur")
+elif dayInWeek == 4:
+  print("Miðvikudagur")
+elif dayInWeek == 5:
+  print("Fimmtudagur")
+elif dayInWeek == 6:
+  print("Föstudagur")
+elif dayInWeek == 7:
+  print("Laugardagur")
+else:
+  print("Rangur innsláttur")
