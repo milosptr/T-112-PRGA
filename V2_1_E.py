966872e8a7a08b5383e32497b1a7b17af1cfcc43
Milos Petrovic <milos@sidekickhealth.com>
2023-08-28 16:56:41 +0000

    if first integer adds up to second integer

---
+++ V2_1_E.py
@@ -0,0 +1,13 @@
+# Höfundur: Milos Petrovic
+# Dagsetning: 28.08.2023
+# Verkefni: Forritun 1A - V2_1_E
+# Athugasemdir: Ekki athugasemdir
+# Gast þú leyst þetta verkefni án hjálpar (upptöku eða
+# annars aðila: Já
+
+firstInteger = int(input("Fyrri heiltalan: "))
+secondInteger = int(input("Seinni heiltalan: "))
+if(firstInteger * 2 == secondInteger):
+  print("talan", firstInteger, "gengur upp í", secondInteger)
+else:
+  print("talan", firstInteger, "gengur ekki upp í", secondInteger)
